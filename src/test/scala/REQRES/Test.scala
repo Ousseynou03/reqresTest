934f19f2f280468a599d2bd38f50f03106174fc5
Dione Ousseynou <dioneousseynou03@gmail.com>
2023-05-26 15:32:16 +0100

    first commit

---
+++ src/test/scala/REQRES/Test.scala
@@ -0,0 +1,45 @@
+package REQRES
+
+
+import scala.concurrent.duration._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.recorder.internal.bouncycastle.oer.its.ieee1609dot2.basetypes.Duration.{minutes, seconds}
+
+import scala.language.postfixOps
+
+class Test  extends  Simulation{
+
+  private val host: String = System.getProperty("urlCible", "https://reqres.in")
+
+
+  val httpProtocol =   http
+    .baseUrl(host)
+    .acceptHeader("application/json")
+
+  val scn = scenario("Test d'API")
+    .exec(
+      http("Obtenir les détails de l'utilisateur")
+      .get("/api/users?page=2")
+        .check(status is 200)
+      .check(jsonPath("$.data[0].id").saveAs("userId")))
+    .exec { session =>
+      val userId = session("userId").as[String]
+      session.set("id", userId)
+    }
+    .exec { session =>
+      println("id = " + session("id").as[String])
+      session
+    }
+    .exec(http("Obtenir l'utilisateur par ID")
+      .get("/api/users/${id}")
+  )
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpProtocol)
+
+
+
+
+}
